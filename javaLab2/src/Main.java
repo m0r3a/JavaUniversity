af1d77af7a5c7c3224576c2ba12f91e825d30fff
m0r3a <buretdan52@gmail.com>
2023-11-20 11:34:24 +0200

    Second commit

--- javaLab2/src/Main.java
+++ javaLab2/src/Main.java
@@ -5,14 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            // Display the menu
             System.out.println("Select an option:");
             System.out.println("1. Find common items in two arrays");
             System.out.println("2. Generate Fibonacci sequence");
             System.out.println("3. Check for palindrome");
             System.out.println("4. Exit");
 
-            // Get user choice
             int choice = scanner.nextInt();
 
             switch (choice) {
@@ -37,15 +35,12 @@ public class Main {
     private static void findCommonItems() {
         Scanner scanner = new Scanner(System.in);
 
-        // Input for the first array
         System.out.print("Enter elements for the first array (comma-separated): ");
         String[] array1 = scanner.nextLine().split(",");
 
-        // Input for the second array
         System.out.print("Enter elements for the second array (comma-separated): ");
         String[] array2 = scanner.nextLine().split(",");
 
-        // Find and display common items
         System.out.print("Common items in both arrays: ");
         for (String item : array1) {
             for (String item2 : array2) {
@@ -61,11 +56,9 @@ public class Main {
     private static void generateFibonacci() {
         Scanner scanner = new Scanner(System.in);
 
-        // Input for the limit
         System.out.print("Enter the limit for Fibonacci sequence: ");
         int limit = scanner.nextInt();
 
-        // Generate Fibonacci sequence
         int a = 0, b = 1, c;
         System.out.print("Fibonacci sequence up to " + limit + ": ");
         System.out.print(a + " " + b + " ");
@@ -80,14 +73,11 @@ public class Main {
     private static void checkPalindrome() {
         Scanner scanner = new Scanner(System.in);
 
-        // Input for the string to check
         System.out.print("Enter a string to check for palindrome: ");
         String input = scanner.nextLine();
 
-        // Remove spaces and convert to lowercase for case-insensitive comparison
         String cleanedInput = input.replaceAll("\\s", "").toLowerCase();
 
-        // Check for palindrome
         String reversed = new StringBuilder(cleanedInput).reverse().toString();
         if (cleanedInput.equals(reversed)) {
             System.out.println(input + " is a palindrome.");
